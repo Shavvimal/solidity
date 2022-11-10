088ab1ef59394babdcd7e3cf6e6538f9783d1e24
shavvimalND <108663701+shavvimalND@users.noreply.github.com>
2022-11-10 20:13:21 +0000

    paywall message board

---
+++ MessengeboardPaywall/MessengeboardPaywall.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.8.17;
+
+contract MessengeboardPaywall {
+    string public message;
+    address payable deployeAddress;
+
+    constructor() {
+        message = "Hello World";
+        deployeAddress = payable(msg.sender);
+    }
+
+    function setMessage(string memory newMessage) payable public {
+        if (msg.value > 1 ether) {
+            message = newMessage;
+            deployeAddress.transfer(msg.value);
+        } else {
+            payable(msg.sender).transfer(msg.value);
+            revert("Not enough donated.... Returning Funds");
+        }
+    }
+}
---
+++ MessengeboardPaywall/README.md
@@ -0,0 +1,7 @@
+# Blockchain Messenger w/ paywall
+
+This contract will:
+
+- Save a message on the blockchain readable to everyone _IF_ the sender sends us 1 ETH or more.
+- If insufficient funds are sent. the ETH is returned to the sender
+- All money over threshold is forwarded to the person who created the contract
